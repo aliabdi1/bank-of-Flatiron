27ce62108df2391f3a3549e84fb6d517b3ef2dd5
Ali Abdkadir <ali.abdikadir@student.moringaschool.com>
2024-08-11 15:07:00 +0300

    done

---
+++ bank-of-flatiron/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Trans from './components/transactions';
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+    <Trans/>
+  </div>
+  
+
+  );
+}
+
+export default App;
